5bb2476b321dbd57f1094e66281fb6ecd1c56fb1
Clint Remedios <clint.remedios@visioncritical.com>
2015-11-05 18:40:52 -0800

    changed ibeacon name

--- js/locations.js
+++ js/locations.js
@@ -1,13 +1,13 @@
 var locations =
 [
 	{
-		name: 'Human Resources',
+		name: 'R & D',
 		uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
 		major: 50320,
 		minor: 57488
 	},
 	{
-		name: 'R & D',
+		name: 'Human Resources',
 		uuid:'2f234454-cf6d-4a0f-adf2-f4911ba9ffa6',
 		major: 1,
 		minor: 2
